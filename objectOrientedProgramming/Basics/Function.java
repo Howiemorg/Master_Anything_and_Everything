2c3a9b64ad948725762ccfbbc627d971cead8d1c
Howiemorg <morgenthalerhowie@yahoo.com>
2024-05-02 01:16:16 -0500

    Created file to showcase functions/methods

---
+++ objectOrientedProgramming/Basics/Function.java
@@ -0,0 +1,22 @@
+package Basics;
+
+public class Function {
+    // "Public" is a keyword, we'll get into that later
+    // "int" is the return type of the function/method
+    // "add" is the name of the method
+    // "int x" and "float y" are the parameters and types of the parameters
+    // the parameters are values passed into the functions
+    public double add(int x, float y){
+        // "return" is a keyword that sends what comes after it back to the function caller
+        // so x + y gets sent back to what calls the function
+        return x + y;
+    }
+    public static void main(String[] args) {
+        int integer = 2;
+        float floatDecimal = -3.45f;
+
+        Function func = new Function();
+        double returnValue = func.add(integer, floatDecimal);
+        System.out.println(returnValue);
+    }
+}
