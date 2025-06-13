f33c328db1faa8361fc907893136abe6e84bb3df
Howiemorg <morgenthalerhowie@yahoo.com>
2025-06-12 19:01:07 -0500

    Added C++ version of Basics

---
+++ Basics/Basics.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 11;
+    cout << x << endl;
+    int y = x * 6;
+    cout << y << endl;
+
+    // if(x != 8) {
+    //     cout << "Good" << endl;
+    // }
+    // else if(x > 10) {
+    //     y = 8;
+    //     cout << y << endl;
+    // }
+    // else {
+    //     cout << "Bad" << endl;
+    // }
+        
+    // for(int i = 0; i < 5; i++) {
+    //     cout << i << endl;
+    // }
+
+    // for(int i : {1,2,3,4,5}){
+    //     cout << i << endl;
+    // }
+
+    // while(x != 0) {
+    //      cout << x << endl;
+    // }
+
+    // while(x != 0) {
+    //     x = x - 1;
+    //     cout << x << endl;
+    // }
+}
